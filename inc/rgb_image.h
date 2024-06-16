c5cab48f68cb3ee0c2d1b830204b295d2fb2ecc8
ayanaxinnn <164034200+ayanaxinnn@users.noreply.github.com>
2024-06-17 00:42:12 +0800

    Update rgb_image.h

--- inc/rgb_image.h
+++ inc/rgb_image.h
@@ -2,12 +2,31 @@
 #define _RGB_IMAGE_H_
 
 #include "image.h"
+#include <string>
 
-class RGBImage : public Image{
+class RGBImage : public Image {
 private:
+    int*** pixels;
 
 public:
+    RGBImage();
+    RGBImage(int width, int height, int*** pixels);
+    ~RGBImage();
 
+    bool LoadImage(std::string filename);
+    void DumpImage(std::string filename);
+    void Display_X_Server();
+    void Display_ASCII();
+    void Display_CMD();
+
+    void set_height(int h);
+    void set_width(int w);
+    void PreviousDelete();
+    void SetPrivate(int w, int h, int*** pixel);
+    int get_pixel(int x, int y, int c) const;
+    void set_pixel(int x, int y, int c, int value);
+    
+    void initial(int w,int h);
 };
 
 #endif
