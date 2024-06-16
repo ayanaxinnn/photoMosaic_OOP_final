e61c53f59ba0d1472526ae1dc9d5dc2a5ab95793
ayanaxinnn <164034200+ayanaxinnn@users.noreply.github.com>
2024-06-17 00:41:39 +0800

    Update photo_mosaic.h

--- inc/photo_mosaic.h
+++ inc/photo_mosaic.h
@@ -1,8 +1,28 @@
 #ifndef _PHOTO_MOSAIC_H_
 #define _PHOTO_MOSAIC_H_
 
-class PhotoMosaic{
-
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include "rgb_image.h"
+#include "image.h"
+#include"data_loader.h"
+class PhotoMosaic {
+private: 
+    std::vector<Image*> smallImage;
+    RGBImage BigImage;
+    //int GCD;
+    int kernalSize_h;
+    int kernalSize_w;
+public:
+    PhotoMosaic();
+    PhotoMosaic(const std::string targetImageFile);
+    void InputsmallImage(const std::string smallImageFilename);
+    //void GCDofBigImage();
+    void NormalizesmallImage();
+    int CalculateDifference(int h, int w);
+    void resultMosaic(RGBImage& Final);
 };
 
 #endif
